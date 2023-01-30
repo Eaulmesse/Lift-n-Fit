ce0ecf44af859c76eac1e1910fa70a619ae93eca
Eaulmesse <theo.guerin01120@gmail.com>
2023-01-30 17:34:19 +0100

    reset password + mailer

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -49,18 +49,18 @@ class RegistrationController extends AbstractController
             // generate a signed url and email it to the user
             $this->emailVerifier->sendEmailConfirmation('app_verify_email', $user,
                 (new TemplatedEmail())
-                    ->from(new Address("lift'n'fit@gmail.com", 'Lift\'N\'Fit Bot'))
+                    ->from(new Address("liftnfit@mail.com", 'LiftNFit Bot'))
                     ->to($user->getEmail())
                     ->subject('Please Confirm your Email')
                     ->htmlTemplate('registration/confirmation_email.html.twig')
             );
             // do anything else you need here, like send an email
 
-            return $userAuthenticator->authenticateUser(
-                $user,
-                $authenticator,
-                $request
-            );
+            // return $userAuthenticator->authenticateUser(
+            //     $user,
+            //     $authenticator,
+            //     $request
+            // );
         }
 
         return $this->render('registration/register.html.twig', [
